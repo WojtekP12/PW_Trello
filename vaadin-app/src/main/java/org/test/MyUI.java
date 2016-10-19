de0ddb4f328fd3d83c2b9b5902325645288fd7b1
Pelka <wojciech.pelka@accenture.com>
2016-10-19 20:44:51 +0200

    project added - first shell

---
+++ vaadin-app/src/main/java/org/test/MyUI.java
@@ -0,0 +1,77 @@
+package org.test;
+
+import javax.servlet.annotation.WebServlet;
+
+import com.vaadin.annotations.Theme;
+import com.vaadin.annotations.VaadinServletConfiguration;
+import com.vaadin.server.VaadinRequest;
+import com.vaadin.server.VaadinServlet;
+import com.vaadin.ui.*;
+
+/**
+ * This UI is the application entry point. A UI may either represent a browser window 
+ * (or tab) or some part of a html page where a Vaadin application is embedded.
+ * <p>
+ * The UI is initialized using {@link #init(VaadinRequest)}. This method is intended to be 
+ * overridden to add component to the user interface and initialize non-component functionality.
+ */
+@Theme("mytheme")
+public class MyUI extends UI {
+
+    @Override
+    protected void init(VaadinRequest vaadinRequest) {
+        final VerticalLayout layout = new VerticalLayout();
+        layout.addStyleName("mainContainerStyle");
+        layout.setHeight("100%");
+        setContent(layout);
+
+        GridLayout titleBar = new GridLayout(30,1);
+        titleBar.setSpacing(true);
+        titleBar.setWidth("100%");
+        titleBar.addStyleName("titleBarStyle");
+        layout.addComponents(titleBar);
+
+        Button boardButton = new Button("Boards");
+        boardButton.setWidth("100%");
+        boardButton.addStyleName("titleBarButtonStyle");
+        titleBar.addComponent(boardButton,0,0,1,0);
+
+        TextField searchArea = new TextField();
+        searchArea.setWidth("100%");
+        searchArea.setHeight("100%");
+        searchArea.addStyleName("titleBarTextFieldStyle");
+        titleBar.addComponent(searchArea,2,0,4,0);
+
+        Label title = new Label("TRELLO");
+        title.setWidth("100%");
+        title.addStyleName("titleBarLabelStyle");
+        titleBar.addComponent(title,5,0,20,0);
+
+        Button addButton = new Button("+");
+        addButton.setWidth("100%");
+        addButton.addStyleName("titleBarButtonStyle");
+        titleBar.addComponent(addButton,21,0,21,0);
+
+        Button userButton = new Button("User Name");
+        userButton.setWidth("100%");
+        userButton.addStyleName("titleBarButtonStyle");
+        titleBar.addComponent(userButton,22,0,24,0);
+
+        Button infoButton = new Button("i");
+        infoButton.setWidth("100%");
+        infoButton.addStyleName("titleBarButtonStyle");
+        titleBar.addComponent(infoButton,25,0,25,0);
+
+        Button notificationButton = new Button("Notifications");
+        notificationButton.setWidth("100%");
+        notificationButton.addStyleName("titleBarButtonStyle");
+        titleBar.addComponent(notificationButton,26,0,29,0);
+
+
+    }
+
+    @WebServlet(urlPatterns = "/*", name = "MyUIServlet", asyncSupported = true)
+    @VaadinServletConfiguration(ui = MyUI.class, productionMode = false)
+    public static class MyUIServlet extends VaadinServlet {
+    }
+}
