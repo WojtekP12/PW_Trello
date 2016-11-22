ff3e55d3084a1530c87559ecb2c2c2e10eb40904
MichalKubik <michal.kubik27@gmail.com>
2016-11-22 15:33:05 +0100

    leaving team implemented

--- vaadin-app/src/main/java/org/controls/TeamBoardContainer.java
+++ vaadin-app/src/main/java/org/controls/TeamBoardContainer.java
@@ -2,6 +2,8 @@ package org.controls;
 
 import com.vaadin.ui.HorizontalLayout;
 import com.vaadin.ui.Button;
+import com.vaadin.ui.Button.ClickEvent;
+import com.vaadin.server.Page;
 import org.models.Team;
 import org.models.User;
 
@@ -21,6 +23,18 @@ public class TeamBoardContainer extends BoardContainer
 		
 		Button leaveButton = new Button("Leave");
 		getTitleBar().addComponent(leaveButton);
+		leaveButton.addClickListener(new Button.ClickListener()
+		{
+			public void buttonClick(ClickEvent event)
+			{
+				team.getMembers().remove(User.getUserFromSession());
+				if(team.getAdmins().contains(User.getUserFromSession()))
+					team.getAdmins().remove(User.getUserFromSession());
+				if(team.getAdmins().isEmpty())
+					Team.teamsList.remove(team);
+				Page.getCurrent().reload();
+			}
+		});
 		
 		getTitleBar().setSpacing(true);		
 		
